1ab559f37d930f86e777a1130f5a2c0d2e149cf1
nawad0 <doarmax.max@gmail.com>
2024-05-14 16:41:48 +0300

    Реализация всех сервисов

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class OrderService : IOrderService
+{
+    private readonly ApplicationDbContext _dbContext;
+    public OrderService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<Order> GetOrder()
+    {
+        return await _dbContext.Orders.AsNoTracking().Where(o => o.Quantity > 1)
+            .OrderByDescending(o => o.CreatedAt).FirstOrDefaultAsync() ?? throw new Exception("Order not found");
+    }
+
+    public async Task<List<Order>> GetOrders()
+    {
+        return await _dbContext.Orders.AsNoTracking().Where(u => u.User.Status == UserStatus.Active)
+            .OrderByDescending(o => o.CreatedAt)
+            .ToListAsync();
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class UserService : IUserService
+{
+    private readonly ApplicationDbContext _dbContext;
+    public UserService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<User> GetUser()
+    {
+        return await _dbContext.Users.AsNoTracking()
+            .Where(u => u.Orders.Any(o => o.CreatedAt.Year == 2003 && o.Status == OrderStatus.Delivered))
+            .OrderByDescending(u => u.Orders.Sum(o => o.Quantity)).FirstOrDefaultAsync() ?? throw new Exception("User not found");
+    }
+    public async Task<List<User>> GetUsers()
+    {
+        return await _dbContext.Users.AsNoTracking()
+            .Where(u => u.Orders.Any(o => o.CreatedAt.Year == 2010 && o.Status == OrderStatus.Paid)).ToListAsync();
+    }
+}
